a58c10849a221925c2863c12ac0b7197e90d1568
Hackon13 <115771866+Hackon13@users.noreply.github.com>
2024-10-14 19:37:27 +0530

    Add ViewsandEvents project

---
+++ ViewsandEvents/app/src/main/java/com/example/viewsandevents/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.viewsandevents;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView t1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        t1= (TextView) findViewById(R.id.t1);
+        b1= (Button)findViewById(R.id.b1);
+
+        b1.setOnClickListener(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        t1.setText("You clicked b1");
+    }
+        public void doSomething (View v) {
+            t1.setText("You clicled b2");
+
+
+    }
+}
