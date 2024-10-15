847564c693e30dd7d4544a6f270bdb3244dc8e28
Hackon13 <115771866+Hackon13@users.noreply.github.com>
2024-10-15 13:17:49 +0530

    Add ExplicitIntent project

---
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/Second.java
@@ -0,0 +1,36 @@
+package com.example.explicitintent;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Second extends AppCompatActivity {
+
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        txt1= (TextView)findViewById(R.id.result);
+
+        Bundle b1= getIntent().getExtras();
+        String s1= b1.getString("user");
+        txt1.setText(s1);
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
