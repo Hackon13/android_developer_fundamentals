cf6bda382a386e7d1e94898bc016bb3735a50d5c
Hackon13 <115771866+Hackon13@users.noreply.github.com>
2024-10-17 21:39:00 +0530

    Add ExpressoUITest project

---
+++ ExpressoUITest/app/src/main/java/com/example/espressouitest/Second.java
@@ -0,0 +1,34 @@
+package com.example.espressouitest;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Second extends AppCompatActivity {
+
+    TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        Bundle b1= getIntent().getExtras();
+        String s1=b1.getString("key1");
+        tv1 = (TextView)findViewById(R.id.result);
+
+        tv1.setText(s1);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
